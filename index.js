c43a759dd5415af9d2d01798a493050254b58c6d
sajaadsyntax <nozanteam@gmail.com>
2024-08-03 16:30:25 +0300

    updated rate limit and CORS headers

--- index.js
+++ index.js
@@ -9,6 +9,18 @@ const bodyparser = require('body-parser');
 const cookieParser = require('cookie-parser');
 require('dotenv').config()
 const adminRouter = require('./routes/admin.routes');
+const { rateLimit } = require('express-rate-limit');
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	// store: ... , // Redis, Memcached, etc. See below.
+})
+
+// Apply the rate limiting middleware to all requests.
+app.use(limiter)
 mongoose.connect(MONGO_DB_CONFIG.DB, {
 }).then(
     () => {
