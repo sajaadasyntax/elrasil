dbd242e85a24d10dda5ae66eb2b231e6ad05a5fb
sajaadasyntax <162808327+sajaadasyntax@users.noreply.github.com>
2024-08-05 23:32:20 +0300

    Delete _util directory

--- _util/validatemongodbID.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const validateMongodbid = (id) => {
-const isValid = mongoose.Types.ObjectId.isValid(id);
-if (!isValid) {
- throw new Error ('Invalid ID');   
-};
-};
-module.exports = validateMongodbid;
